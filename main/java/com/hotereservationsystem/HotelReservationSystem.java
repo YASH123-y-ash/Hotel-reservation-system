151b8f767dfcc3d15c49169350e164a5a21aa424
Yash-y-ash <wardhan696@gmail.com>
2021-04-16 08:52:20 +0530

    Added:to find cheapest hotel in given date

--- main/java/com/hotereservationsystem/HotelReservationSystem.java
+++ main/java/com/hotereservationsystem/HotelReservationSystem.java
@@ -1,27 +1,49 @@
 package com.hotereservationsystem;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Scanner;
 
 public class HotelReservationSystem {
 
     private ArrayList<Hotel> data=new ArrayList<Hotel>();
 
-    static Scanner sc ;
-
         //method to add hotel details
-        public void addHotelDetails(){
-            System.out.println("enter hotel name");
-            String hotelName= sc.next();
-            System.out.println("enter rates for regular customer");
-            int ratesForRegularcustomer = sc.nextInt();
-            Hotel newEntry=new Hotel(hotelName, ratesForRegularcustomer);
-
-            this.data.add(newEntry);
+        public boolean addHotel(){
+            Hotel hotel = new Hotel();
+            boolean added = data.add(hotel);
+            return added;
         }
-        
+
+        public boolean addHotel(String hotelName,int ratesForRegularCustomer)
+        {
+            Hotel hotel = new Hotel();
+            boolean added = data.add(hotel);
+            return added;
+        }
+
+        //method to find cheapest hotel
+        public String cheapestHotel(Date[] dates)
+        {
+            ArrayList<Integer> cheapHotel = new ArrayList<>();
+            for (Hotel hotel : data)
+            {
+                int rate = 0;
+                for (Date date : dates)
+                {
+                    rate = hotel.getratesForRegularCustomer();
+                }
+                cheapHotel.add(rate);
+            }
+            Integer cheapRate = cheapHotel.stream().min(Comparator.comparing(Integer :: intValue)).orElse(null);
+            int position = cheapHotel.indexOf(cheapRate);
+            return data.get(position).getHotelName();
+        }
+
     public static void main(String[] args) {
         HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
-        hotelReservationSystem.addHotelDetails();
+        hotelReservationSystem.addHotel();
     }
 }
--- test/java/com/hotelreservationsystem/HotelReservationSystemTest.java
+++ test/java/com/hotelreservationsystem/HotelReservationSystemTest.java
@@ -5,14 +5,44 @@ import com.hotereservationsystem.HotelReservationSystem;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HotelReservationSystemTest {
 
+    HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
     @Test
-    //checking if given hotel object are matched return true
+    //checking if given hotel object are added return true
     public void givenHotelObjectShouldMatch_ReturnTrue()
     {
-        Hotel hotel = new Hotel("lakewood",220);
-        Assertions.assertEquals(hotel.showHotelDetails();)
+        boolean check = hotelReservationSystem.addHotel("lakewood",100);
+        Assertions.assertTrue(check);
+    }
 
+    @Test
+    //checking for cheapest hotel
+    public void givenDate_ShouldFindCheapestHotel()
+        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMMMYYYY");
+        String date[] = {"10sep2020","11sep2020"};
+        Date[] dates = null;
+        try{
+            dates = new Date[]{dateFormat.parse(date[0]), dateFormat.parse(date[1])};
+    }
+        catch(ParseException exception)
+    {
+        System.out.println("date pattern is invalid");
+        if (!dateFormat.format(date).equals(dates)){
+            System.out.println("date is invalid");
+        }
+        else {
+            System.out.println("date is valid");
+
+        boolean check = hotelReservationSystem.addHotel("Ridgewood",220)&&
+                hotelReservationSystem.addHotel("lakewood",100);
+        if(check)
+            Assertions.assertEquals("lakewood",hotelReservationSystem.cheapestHotel(dates));
+
+        }
     }
 }
